0429c0a9a8928ef82288d84b9c683a5eef4bb1d2
Bond4webster <slawa-bondarev2012@yandex.ru>
2020-01-13 17:10:49 +0300

    add cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const swaggerDocument = require('./swagger');
 const userRouter = require('./routers/UserRouter');
 const cityRouter = require('./routers/CityRouter');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 mongoose.connect('mongodb://localhost:27017/Users', {
     useNewUrlParser: true,
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(userRouter);
 app.use(cityRouter);
+app.use(cors());
 app.listen(3000,function(){
     console.log('App listening on port 3000!');
 });
--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -11,7 +11,7 @@ const userRouter = new express.Router();
 userRouter.get('/User',auth,user_controller.getAllUsers);
 userRouter.get('/User/:id',auth,user_controller.getUser);
 userRouter.get('/User/City/:id',user_controller.getUserWithCity);
-userRouter.post('/User',validation(userValidation),user_controller.addUser);
+userRouter.post('/User/reg',validation(userValidation),user_controller.addUser);
 userRouter.post('/User/login',user_controller.loginUser);
 userRouter.post('/User/logout',auth,user_controller.logoutUser);
 userRouter.put('/User/:id',auth,validation(userValidation),user_controller.updateUser);
